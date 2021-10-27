b0f2021a259931e07c9539bd677e609f494d124b
911_Alexandra_Bledea <bledea_alexandra@yahoo.com>
2021-10-27 13:51:08 +0300

    Final Version
    This project is an application in which we have some users at some sort of a contest. They can answer questions, display the answers for each question with yellow background for they own answers, vote an answer for a question and add another question.

---
+++ Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Domain/Answer.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Domain/Answer.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Domain/Question.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Domain/Question.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Domain/User.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Domain/User.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/Question_and_answer_site_autogen/mocs_compilation.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Question_and_answer_site_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Repository/AnswerRepo.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Repository/AnswerRepo.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Repository/QuestionRepo.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Repository/QuestionRepo.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Repository/UserRepo.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Repository/UserRepo.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/Service/Service.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/Service/Service.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/TableModel/TableModel.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/TableModel/TableModel.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/UserWindow/UserWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/UserWindow/UserWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/main.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Question and answer site/cmake-build-debug/CMakeFiles/Question_and_answer_site.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Question_and_answer_site_autogen/include"
+  "../Domain"
+  "../Repository"
+  "../Service"
+  "../Observer"
+  "../UserWindow"
+  "../TableModel"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
