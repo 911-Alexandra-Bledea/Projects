f9de9b3e03a54cf3eb02271fe1db10e0fe7757be
911_Alexandra_Bledea <bledea_alexandra@yahoo.com>
2021-10-27 12:54:08 +0300

    Final Version
    The project is an application which allow programmers to revise other programmers files. Everything is updated in real time. Each programmer normally has a goal number of revised files. Programmers can also add new files for revising. The application also presents a statistics window, regarding to the number of files each programmer revised.

---
+++ Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/Code_revision_autogen/mocs_compilation.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/Code_revision_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/Programmer/Programmer.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/Programmer/Programmer.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/ProgrammerWindow/ProgrammerWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/ProgrammerWindow/ProgrammerWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/RepoFiles/Files.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/RepoFiles/Files.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/RepoProgrammers/RepoProgrammers.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/RepoProgrammers/RepoProgrammers.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/SourceFile/SourceFile.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/SourceFile/SourceFile.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/StatisticsWindow/StatisticsWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/StatisticsWindow/StatisticsWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/TableModel/TableModel.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/TableModel/TableModel.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/main.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Code revision/cmake-build-debug/CMakeFiles/Code_revision.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Code_revision_autogen/include"
+  "../Programmer"
+  "../SourceFile"
+  "../RepoFiles"
+  "../RepoProgrammers"
+  "../Observer"
+  "../ProgrammerWindow"
+  "../TableModel"
+  "../StatisticsWindow"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
