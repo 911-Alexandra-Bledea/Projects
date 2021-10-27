81b588cf9d935c90f0a0870910d374545bb1b3f9
911_Alexandra_Bledea <bledea_alexandra@yahoo.com>
2021-10-27 13:02:59 +0300

    Final Version
    This project represent a multiple user application, where we have testers and programmers. Programmers can resolve the issues. Testers can report issues. Both of them can remove closed issues. Everything is updated in real time for each user window.

---
+++ Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/Develpoment_Team_autogen/mocs_compilation.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/Develpoment_Team_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/Issue/Issue.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/Issue/Issue.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/Member/Member.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/Member/Member.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/MemberWindow/MemberWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/MemberWindow/MemberWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/Repository/Repository.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/Repository/Repository.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/Service/Service.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/Service/Service.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/TableModel/TableModel.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/TableModel/TableModel.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/main.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Develpoment Team/cmake-build-debug/CMakeFiles/Develpoment_Team.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Develpoment_Team_autogen/include"
+  "../Issue"
+  "../Member"
+  "../Repository"
+  "../Service"
+  "../Observer"
+  "../TableModel"
+  "../MemberWindow"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
