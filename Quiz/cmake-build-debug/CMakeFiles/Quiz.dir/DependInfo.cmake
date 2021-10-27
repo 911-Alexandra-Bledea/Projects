97253ba78435107d09ac127bd97363a6743c6d91
911_Alexandra_Bledea <bledea_alexandra@yahoo.com>
2021-10-27 13:54:44 +0300

    Final Version
    This project is an application which represents a quiz. We have a presenter and more contestants. Each contestant can answer each question once. Each question has a score and also the contestants have a total score. For each correct answer their total score increases. The presenter can add new questions and can see the correct answers for all the other questions.

---
+++ Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/DependInfo.cmake
@@ -0,0 +1,52 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/Participant/Participant.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/Participant/Participant.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/ParticipantWindow/ParticipantWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/ParticipantWindow/ParticipantWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/PresenterWindow/PresenterWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/PresenterWindow/PresenterWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/Question/Question.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/Question/Question.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/Quiz_autogen/mocs_compilation.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/Quiz_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/Repository/RepoParticipants.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/Repository/RepoParticipants.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/Repository/RepoQuestions.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/Repository/RepoQuestions.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/TableModel/TableModel.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/TableModel/TableModel.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/TableModel/TableModelPresenter.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/TableModel/TableModelPresenter.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/main.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/Quiz/cmake-build-debug/CMakeFiles/Quiz.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "Quiz_autogen/include"
+  "../Participant"
+  "../Question"
+  "../Repository"
+  "../Observer"
+  "../TableModel"
+  "../ParticipantWindow"
+  "../PresenterWindow"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
