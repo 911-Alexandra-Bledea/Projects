38bcee3297ebcb077159e4a07794ed315fdc8cd0
911_Alexandra_Bledea <bledea_alexandra@yahoo.com>
2021-10-27 13:57:30 +0300

    Final Version
    The project represents a screenwriting application. We have more screenwriters which can add an idea, accept proposed ideas, develop ideas and the save them, and also save an entire plot to a file.

---
+++ ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/DependInfo.cmake
@@ -0,0 +1,49 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/Domain/Idea.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/Domain/Idea.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/Domain/ScreenWriter.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/Domain/ScreenWriter.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/GUI/DevelopWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/GUI/DevelopWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/GUI/ScreenWriterWindow.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/GUI/ScreenWriterWindow.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/Repository/IdeasRepo.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/Repository/IdeasRepo.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/Repository/ScreenWritersRepo.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/Repository/ScreenWritersRepo.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/ScreenWriting_autogen/mocs_compilation.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/ScreenWriting_autogen/mocs_compilation.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/TableModel/TableModel.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/TableModel/TableModel.cpp.obj"
+  "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/main.cpp" "C:/Users/Alexandra/Desktop/Facultate/Semestru 2/EXAMEN OOP/ScreenWriting/cmake-build-debug/CMakeFiles/ScreenWriting.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CHARTS_LIB"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "ScreenWriting_autogen/include"
+  "../Domain"
+  "../Repository"
+  "../Observer"
+  "../GUI"
+  "../TableModel"
+  "C:/Qt/5.15.2/mingw81_64/include"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCore"
+  "C:/Qt/5.15.2/mingw81_64/./mkspecs/win32-g++"
+  "C:/Qt/5.15.2/mingw81_64/include/QtCharts"
+  "C:/Qt/5.15.2/mingw81_64/include/QtWidgets"
+  "C:/Qt/5.15.2/mingw81_64/include/QtGui"
+  "C:/Qt/5.15.2/mingw81_64/include/QtANGLE"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
